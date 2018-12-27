70e938e35d613d997e96d4c6b60100bdf6017e17
raviza22 <raviza22@gmail.com>
2018-12-27 15:32:00 +0530

    added webEventlisteners class

--- src/main/java/com/test/util/TestUtil.java
+++ src/main/java/com/test/util/TestUtil.java
@@ -1,13 +1,17 @@
 package com.test.util;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 import com.test.base.TestBase;
 
@@ -54,6 +58,13 @@ public class TestUtil extends TestBase{
 		}
 		return data;
 		
+		}
+	
+	public static void takeScreenshotAtEndOfTest() throws IOException{
+		
+		File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		String currentDir = System.getProperty("user.dir");
+		FileUtils.copyFile(scrFile, new File(currentDir + "\\Screenshots\\" + System.currentTimeMillis() + ".png"));
 		
 	}
 }
